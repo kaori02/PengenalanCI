945a956e841fa2b9a0808645b71062b704ba4765
kaori02 <kaorigaming02@gmail.com>
2021-06-15 02:20:26 +0700

    gabisa delete halp :((

--- application/views/admin/portofolio/list.php
+++ application/views/admin/portofolio/list.php
@@ -92,7 +92,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 											<?= substr($portofolio->portofolio_desc, 0, 120) ?>...</td>
 										<td width="250">
 											<a href="<?= base_url('admin/portofolios/edit/'.$portofolio->portofolio_id) ?>" class="btn btn-warning btn-block">Edit</a>
-											<a onclick="deleteConfirm('<?= base_url('admin/portofolios/delete/'.$portofolio->portofolio_id) ?>')" href="#!" class="btn btn-danger btn-block">Delete</a>
+											<a onclick="deleteConfirm('<?php echo site_url('admin/portofolios/delete/'.$portofolio->portofolio_id) ?>')" href="#!" class="btn btn-danger btn-block">Delete</a>
 										</td>
 									</tr>
 									<?php endforeach; ?>
@@ -110,16 +110,35 @@ defined('BASEPATH') OR exit('No direct script access allowed');
           <small>Copyright &copy; 2020 - Dafa Berlian.</small>
       </div>
   </footer>
-<script>
-	$(document).ready( function () {
-    $('#dataTable').DataTable();
-} );
+	
+	<!-- Logout Delete Confirmation-->
+	<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="exampleModalLabel">Are you sure?</h5>
+					<button class="close" type="button" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">×</span>
+					</button>
+				</div>
+				<div class="modal-body">Data yang dihapus tidak akan bisa dikembalikan.</div>
+				<div class="modal-footer">
+					<button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+					<a id="btn-delete" class="btn btn-danger" href="#">Delete</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<script>
+		$(document).ready( function () {
+			$('#dataTable').DataTable();
+	} );
 
-function deleteConfirm(url){
-	$('#btn-delete').attr('href', url);
-	$('#deleteModal').modal();
-}
-</script>
+	function deleteConfirm(url){
+		$('#btn-delete').attr('href', url);
+		$('#deleteModal').modal();
+	}
+	</script>
 </body>
 
 </html>
